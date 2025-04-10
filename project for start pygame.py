48bd168e1ef95070b2b18240be2694baf53bc032
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2025-04-10 16:49:03 +0100

    Add files via upload

---
+++ project for start pygame.py
@@ -0,0 +1,9 @@
+import pygame
+
+# Intialize pygame and screen dimensions
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+# Initialize display and set title
+display_surface = pygame.dis.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Adding image annd background image')
