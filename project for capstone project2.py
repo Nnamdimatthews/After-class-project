1d8621e5eb666befb329eba4a7813f84a8c94c94
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2025-04-13 16:24:08 +0100

    Add files via upload

---
+++ project for capstone project2.py
@@ -0,0 +1,17 @@
+print("welcome to this simple calculator")
+
+# enter your numbers🔢
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+# number signs ➕➖✖️➗
+Addition = num1+num2
+Substraction = num1-num2
+Multiplication = num1*num2
+Division = num1/num2
+
+# What is the answer🤔❔
+print("The two numbers Added together is, ", Addition)
+print("The two numbers Substracted from eachother is, ", Substraction)
+print("The two numbers Multiplied together is, ", Multiplication)
+print("The two numbers Divided is, ", Division)
