0a0281e6efdaa23525af659e58a942e19f6c210d
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2025-04-21 14:29:43 +0100

    Add files via upload

---
+++ object more.py
@@ -0,0 +1,13 @@
+class fruit:
+
+  def __init__(self, name, color):
+    self.name = name
+    self.color = color
+
+  # instance method🤷🏾‍♂️
+  def intro(self):
+    print("hello, I am", self.name)
+# object creation🥏
+Hedgehog = ('sonic','blue')
+# calling function
+Hedgehog.intro
