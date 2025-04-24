76fcf7080e12075227745a3229b6225c8fd4b176
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2025-04-24 15:10:35 +0100

    Add files via upload

---
+++ project for advancey py-r.py
@@ -0,0 +1,9 @@
+ages=[12, 45, 19, 78, 67]
+for age in ages:
+    if age < 45:
+        print(age, "not allowed")
+        # use exit function⚡
+        print(exit)
+        exit()
+    else:
+        print(age, "allowed💁🏾")
